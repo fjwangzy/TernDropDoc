da060c4f39ef64136188e166a2948ad701e6ca04
fjwangzy <45187355@qq.com>
2026-02-10 14:05:50 +0800

    aaa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const docsDir = path.resolve(__dirname, 'docs')
+const docsDir = path.resolve(__dirname, '..')
 
 // 自动生成导航栏
 function generateNavbar() {
